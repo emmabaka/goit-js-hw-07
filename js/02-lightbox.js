36677737ac62e800959acefa3bbca36a9ec1fae6
emmabaka <bakaemma88@gmail.com>
2023-02-16 13:40:06 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryMarkup = galleryItems
+  .map((img) => {
+    return `<a class="gallery__item" href="${img.original}">
+<img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+</a>`;
+  })
+  .join("");
+
+const gallery = document.querySelector(".gallery");
+gallery.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 200,
+  captionsData: "alt",
+});
