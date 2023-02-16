36f886e3646f880758522415592d989c36ae7f25
emmabaka <bakaemma88@gmail.com>
2023-02-16 13:09:55 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`
+  )
+  .join("");
+
+console.log(galleryMarkup);
+
+gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+gallery.addEventListener("click", onImgClick);
+
+function onImgClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  console.log(e.target.nodeName);
+  const instance = basicLightbox.create(
+    `
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", escClose);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", escClose);
+      },
+    }
+  );
+
+  instance.show();
+}
+
+function escClose() {
+  if (e.code === "Escape") {
+    instance.close();
+  }
+}
